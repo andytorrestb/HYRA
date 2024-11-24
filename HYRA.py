602984ba5960bd505b826ce8b8075d663fc95af8
andytorrestb <andytorrestb@gmail.com>
2024-11-23 20:02:24 -0500

    misc updates

--- HYRA.py
+++ HYRA.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+# Molecular Weight as a Function from the Plotted Graph.
+def get_MW(OF):
+    k = [52.301, -127.7, 150.56, -89.006, 22.23, -4.631, 0.3094]
+    MW = 0
+    for i in range(6):
+        MW = MW + k[i]*OF**i
+    return MW
 
 # Constants from your provided code
 m_dot_ox_i = 20  # kg/s
@@ -10,7 +17,7 @@ P_o = 1.5e6  # Pa
 rho_f = 920  # kg/m^3
 L = 5  # m
 g_o = 9.81  # m/s^2
-dt = 0.1  # Time step in seconds
+dt = 1  # Time step in seconds
 
 # Initialize arrays
 num_points = 20  # Number of spatial points
@@ -18,10 +25,13 @@ L_x = np.linspace(L/40, L*(1 + L/40), num_points)  # Spatial distribution
 R_t = np.full((1, num_points), R_i)  # Initial radii (2D array with 1 row)
 time = [0]  # Time array
 
+MW_t = np.full((1, num_points), 0)  # Initial radii (2D array with 1 row)
+
 # Simulation loop
 i = 0
 while R_t[-1, 0] < R_f: # Compare only the last element in the last row
     R_x = np.zeros(num_points)
+    MW_x = np.zeros(num_points)
     m_dot_ox = m_dot_ox_i
     m_dot_f = 0
     for j, x in enumerate(L_x):
@@ -43,6 +53,10 @@ while R_t[-1, 0] < R_f: # Compare only the last element in the last row
         # Calculate mass to feed up stream
         m_dot_f = rho_f*np.pi*R_x[j]**2*r_dot
 
+        OF = m_dot_ox / m_dot_f
+
+        MW_x[j] = get_MW(OF)
+
     # Append new radii distribution
     R_t = np.vstack([R_t, R_x])
     time.append(time[-1] + dt)
@@ -61,7 +75,6 @@ def init():
     line.set_data([], [])
     return line,
 
-
 def update(frame):
     line.set_data(L_x, R_t[frame])
     current_time = time[frame]  # Get the time corresponding to the frame
@@ -71,6 +84,19 @@ def update(frame):
 anim = FuncAnimation(fig, update, frames=len(R_t), init_func=init, blit=True)
 
 # Save the animation as an MP4 file
-anim.save('radii_evolution.mp4', fps=300, extra_args=['-vcodec', 'libx264'])
+anim.save('radii_evolution.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
 print("Animation saved as radii_evolution.mp4")
+
+
+# Plot fuel grain radii.
+index_vals = [50, 100, 150, 200, i]
+plt.clf()
+
+for i in index_vals:
+    plt.plot(L_x, R_t[i], label = 'time = ' + str(time[i]))
+plt.xlabel("Position (m)")
+plt.ylabel("Radius (m)")
+plt.title("Radii Evolution Over Time")
+plt.legend(loc="upper right")
+plt.savefig('raddii_time_steps.png')
